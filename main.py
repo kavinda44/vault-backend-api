9319c00bb8e6d4f6e1a0373e77e05ddfa876f239
kavinda <kavindadissanayake57@gmail.com>
2026-04-09 01:50:43 +0530

    Update CORS

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ database.init_db()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
